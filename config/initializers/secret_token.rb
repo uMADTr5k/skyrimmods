972fedbce7f0142afbe52f708e004732b9ac7f87
noe <noe.gameshake@gmail.com>
2014-02-15 11:06:57 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skyrimmods::Application.config.secret_key_base = '8e1858cfd9577fc263ed35151426d06fedb993b58e3d4e861d2beaf3a47d588b2c6fdf68d691e1d2a671f16c44d6dbfe47ce8ee10d8dcefc001623f0422b75e6'
